3f3ff54881034f672421bf8f6369c0178a8cf032
Timur Gorodetsky <gorodetsky1392@gmail.com>
2022-06-26 10:51:38 +0600

    add:add result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let oneSymbol = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    let twoSymbol = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    let threeSymbol = [
+        "",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    let result = [];
+
+    let hundreds = Math.floor(number / 100);
+    let dozens = number % 100;
+    let units = number % 10;
+
+    if (number === 0) {
+        result.push(oneSymbol[0]);
+    }
+
+    if (hundreds > 0) {
+        result.push(oneSymbol[hundreds] + " hundred");
+    }
+
+    if (dozens >= 20) {
+        result.push(threeSymbol[Math.floor(dozens / 10)]);
+        if (units !== 0) {
+            result.push(oneSymbol[units]);
+        }
+    } else if (dozens >= 10) {
+        result.push(twoSymbol[Math.floor(dozens % 10)]);
+    } else if (dozens !== 0) {
+        result.push(oneSymbol[units]);
+    }
+    return result.join(" ");
+};
